e9e8a735b00f70fa45ac9dbdb0df904d068f64e5
Anton Gridushko <gridushko.anton@yahoo.com>
2024-04-30 12:09:40 +0300

    Add project files.

---
+++ Remote/Program.cs
@@ -0,0 +1,29 @@
+﻿using Renci.SshNet;
+
+namespace Remote;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		var connectionInfo = new ConnectionInfo("192.168.100.4", "aa",
+			new PasswordAuthenticationMethod("aa", "Ant67299"),
+			new PrivateKeyAuthenticationMethod("rsa.key"));
+
+		using var client = new SshClient(connectionInfo);
+		client.Connect();
+
+		//var isPathContains = client.WorkingDirectory.Contains("aa");
+		//client.DeleteFile("/home/aa/test.sh");
+		//client.RenameFile("/home/aa/test1.sh", "/home/aa/test32.sh");
+		//var isExists = client.Exists("/home/aa/test32.sh");
+		//var files = client.ListDirectory("/home/aa/");
+		//foreach (var item in files)
+		//{
+		//	Console.WriteLine(item.Name);
+		//}
+		//var term = client.RunCommand("ls /home/aa");
+		//string output = term.Result;
+		//Console.WriteLine(output);
+	}
+}
---
+++ Test/Program.cs
@@ -0,0 +1,19 @@
+﻿using Renci.SshNet;
+using System;
+using System.Reflection.PortableExecutable;
+using System.Runtime.Intrinsics.X86;
+using System.Text.RegularExpressions;
+
+namespace Test;
+public class Program
+{
+	public static void Main()
+	{
+		var connectionInfo = new ConnectionInfo("192.168.100.4", "aa",
+			new PasswordAuthenticationMethod("aa", "Ant67299"),
+			new PrivateKeyAuthenticationMethod("rsa.key"));
+
+		using var client = new SshClient(connectionInfo);
+		client.Connect();
+	}
+}
